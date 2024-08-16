5f4d8e061f9a3b4c7ccd417c981cf482287a5422
parthratra11 <parthratra11@gmail.com>
2024-08-17 03:45:23 +0530

    Homepage - Outlets and their menu added

--- canteen-react/src/components/Homepage/homepage.js
+++ canteen-react/src/components/Homepage/homepage.js
@@ -1,3 +1,4 @@
+import { use, useState } from "react";
 import "./homepage.css";
 import { useNavigate } from "react-router-dom";
 
@@ -61,21 +62,50 @@ function NavigationBar() {
       </div>
     </>
   );
-
-  // function globalSearch() {}
 }
 
-function Outlets() {}
+function Outlets({ outlets, setActiveMenu }) {
+  return (
+    <>
+      <div className="outletsNavBar">
+        {outlets.map((outlet) => (
+          <div
+            onClick={() => setActiveMenu(outlet.id)}
+            className="outletsNavItem"
+            key={outlet.id}
+          >
+            {outlet.name}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
-function Menu() {
+function Menu({ outlets, activeMenu }) {
   function search() {}
   function Item() {}
+
+  const outlet = outlets.find((outlet) => outlet.id === activeMenu);
+
+  return <div>{outlet.menu}</div>;
 }
 
 export default function HomePage() {
+  const outlets = [
+    { id: 1, name: "Outlet 1", menu: "Outlet 1 Menu" },
+    { id: 2, name: "Outlet 2", menu: "Outlet 2 Menu" },
+    { id: 3, name: "Outlet 3", menu: "Outlet 3 Menu" },
+    { id: 4, name: "Outlet 4", menu: "Outlet 4 Menu" },
+  ];
+
+  const [activeMenu, setActiveMenu] = useState(1);
+
   return (
     <>
       <NavigationBar />
+      <Outlets outlets={outlets} setActiveMenu={setActiveMenu} />
+      <Menu outlets={outlets} activeMenu={activeMenu} />
     </>
   );
 }
