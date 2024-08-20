62c4e5746610f7d04f46781e8199dd4044d1e9aa
parthratra11 <parthratra11@gmail.com>
2024-08-20 21:19:20 +0530

    xyz

--- server/app.py
+++ server/app.py
@@ -4,13 +4,8 @@ app = Flask(__name__)
 
 import login.loginController
 import registration.registrationController
-<<<<<<< HEAD
-import products.productsController
-
-=======
 
 CORS(app)
->>>>>>> 3ce64e2b86d1af2366eb0a8bc2eccd49a558dfe4
 @app.route("/")
 def welcome():
     return "hello world"
